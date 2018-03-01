c4c45cf3d6e826f53dd06272ef9fc2f26ad24ef5
meng0916 <zhanghuijie0916@gmail.com>
2018-03-01 10:28:57 +0800

    second commit

--- src/main/scala/org/sunny/sparkApp.scala
+++ src/main/scala/org/sunny/sparkApp.scala
@@ -8,12 +8,15 @@ object sparkApp{
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org.apache").setLevel(Level.ERROR)
 
-    val conf = new SparkConf().setAppName("mengapp").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("mengapp").setMaster("local[3]")
     val sc = new SparkContext(conf)
     val text = sc.textFile("hdfs://localhost:9000/huser/test/NOTICE.txt")
-//    val result = text.flatMap(_.split(' ')).map((_, 1)).reduceByKey(_ + _).collect()
-//    result.foreach(println)
-    text.take(10).foreach(println(_))
+    val splitText = text.flatMap(_.split(" "))
+    val filterLenText = splitText.filter(_.length<3).filter(_.length>0)
+    filterLenText.take(20).foreach(println)
+    println("单词长度0-3之间的个数为："+filterLenText.count)
+    println("单词不重复且字符0-3之间的个数为："+filterLenText.distinct.count())
+
 
   }
 }
