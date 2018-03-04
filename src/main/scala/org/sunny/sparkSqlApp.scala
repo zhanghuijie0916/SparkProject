c85dfcf4d1b04524b385f2336e881ba6e6c7daa4
meng0916 <zhanghuijie0916@gmail.com>
2018-03-04 21:01:39 +0800

    second commit

--- src/main/scala/org/sunny/sparkSqlApp.scala
+++ src/main/scala/org/sunny/sparkSqlApp.scala
@@ -17,19 +17,48 @@ object sparkSqlApp {
       .setMaster("local[2]")
     val sc = new SparkContext(conf)
     val spark = SparkSession.builder.config("spark.some.config.option", "some-value").getOrCreate()
-    import spark.implicits._
+    import spark.implicits._ //very important
 
+    /*
+    读取json数据并且转换成Dataset
+     */
     val jsonDF = spark.read.json("hdfs://localhost:9000/huser/resources/people.json").na.fill(0).as[Person]    //Dataset[Person]
     jsonDF.foreach(row=>println(row))
     jsonDF.createTempView("people") //temp table
     spark.sql("SELECT * FROM people where age!=null") //select
+    println("年龄小于20的结果：")
+    jsonDF.select($"age"<19).show  //return true or false
+    println("每个人的年龄增加20的结果：")
+    jsonDF.select($"age"+20).show //每人年纪加上20岁
 
+
+    /*
+    读取parquet数据源
+     */
+    val parquetDF = spark.read.parquet("hdfs://localhost:9000/huser/resources/users.parquet")
+    parquetDF.select($"name".contains("Al"),$"favorite_numbers".as[Array[Int]].isNotNull)
+    /*
+      result：
+      +------------------+------------------------------+
+      |contains(name, Al)|(favorite_numbers IS NOT NULL)|
+      +------------------+------------------------------+
+      |              true|                          true|
+      |             false|                          true|
+      +------------------+------------------------------+
+     */
+
+    /*
+    使用反射推倒Dataset
+     */
     val textRDD = sc.textFile("hdfs://localhost:9000/huser/resources/people.txt")
     val textDF = textRDD.map(line=>line.split(","))
       .map(attributes=>Person(attributes(0),attributes(1).trim.toInt))
       .toDF()
     textDF.map(row=>"Name:"+row.getAs("name")).show()
 
+    /*
+    以编程的方式置顶Schema
+     */
     //创建StructField并将它包裹在StructType中
     val schema = StructType(Array(StructField("name",StringType,true),
       StructField("age",IntegerType,true)))
