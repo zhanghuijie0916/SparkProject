f7769e064884e6072c3018c8a349448b1c3e6642
meng0916 <zhanghuijie0916@gmail.com>
2018-03-06 11:56:22 +0800

    second commit

--- src/main/scala/org/sunny/sparkApp.scala
+++ src/main/scala/org/sunny/sparkApp.scala
@@ -15,7 +15,7 @@ object sparkApp{
       .setMaster("local[2]")
     val sc = new SparkContext(conf)
 
-    //read textFile
+    /*//read textFile
     val text = sc.textFile("hdfs://localhost:9000/huser/test/NOTICE.txt")
     val splitText = text.flatMap(_.split(" ")) //split
     val filterLenText = splitText.filter(_.length<3).filter(_.length>0) //filter
@@ -55,5 +55,44 @@ object sparkApp{
     println(""+sumAccumulator.count+"---"+sumAccumulator.value)
 
     gradesRdd.persist(StorageLevel.MEMORY_ONLY)
+
+    /*
+    RDD aggregate
+    def aggregate[U](zeroValue: U)(seqOp: (U, T) ⇒ U, combOp: (U, U) ⇒ U)(implicit arg0: ClassTag[U]): U
+     seqOp操作会聚合各分区中的元素，然后combOp操作把所有分区的聚合结果再次聚合，两个操作的初始值都是zeroValue.
+     seqOp的操作是遍历分区中的所有元素(T)，第一个T跟zeroValue做操作，结果再作为与第二个T做操作的zeroValue，
+     直到遍历完整个分区。combOp操作是把各分区聚合的结果，再聚合。aggregate函数返回一个跟RDD不同类型的值。
+     因此，需要一个操作seqOp来把分区中的元素T合并成一个U，另外一个操作combOp把所有U聚合。
+     */
+    val rdd1 = sc.parallelize(List(1,2,3,4,5,7,8,9,11),4)
+    rdd1.aggregate((0,0))((result,number)=>(result._1+number,result._2+1),
+      (result1,result2)=>(result1._1+result2._1,result1._2+result2._2))
+    //字符串的aggregate
+    val rdd2 = sc.parallelize(Array("abc","d","ef","g","hig"),4)
+    rdd2.aggregate("")((result,str)=>result+str,_+_)  //结果和分区数有关
+*/
+    //RDD groupBy
+    def my(x:Int):Boolean = x match {
+      case x if x>3 => true
+      case x if x<=3 => false
+    }
+    val rdd3 = sc.parallelize(List(1,2,3,4,5,7,8,9,11))
+    val res = rdd3.groupBy(my)
+    res.foreach(println)
+    //取出RDD中某一个元素
+    val num = res.count()
+    val rddList = res.take(num.toInt) //将RDD转为Array
+
+    //keyBy：为数据集中每个个体数据增加一个key，形成键值对
+    val rdd4 = sc.parallelize(List(1,2,3,4,5,7,8,9,11))
+    val res2 = rdd4.keyBy(_*2)
+
+    val res3 = res2.sortBy(x=>x._1,false) //或者直接使用
+    res3.foreach(println)
+    val res4 = res2.sortByKey()
+    res4.foreach(println)
+
+
+
   }
 }
--- src/main/scala/org/sunny/sparkSqlApp.scala
+++ src/main/scala/org/sunny/sparkSqlApp.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructT
 
 
 object sparkSqlApp {
+  val fileLoc = "hdfs://localhost:9000/huser"
 
   case class Person(name:String,age:BigInt)
 
@@ -22,21 +23,24 @@ object sparkSqlApp {
     /*
     读取json数据并且转换成Dataset
      */
-    val jsonDF = spark.read.json("hdfs://localhost:9000/huser/resources/people.json").na.fill(0).as[Person]    //Dataset[Person]
-    jsonDF.foreach(row=>println(row))
+    val jsonDF = spark.read.json("hdfs://localhost:9000/huser/resources/people.json").na.fill(0).as[Person] //Dataset[Person]
+    jsonDF.foreach(row => println(row))
     jsonDF.createTempView("people") //temp table
     spark.sql("SELECT * FROM people where age!=null") //select
     println("年龄小于20的结果：")
-    jsonDF.select($"age"<19).show  //return true or false
+    jsonDF.select($"age" < 19).show //return true or false
     println("每个人的年龄增加20的结果：")
-    jsonDF.select($"age".cast(IntegerType)+20).show //将年龄强转成Int,并加上20岁
+    jsonDF.select($"age".cast(IntegerType) + 20).show //将年龄强转成Int,并加上20岁
 
 
     /*
     读取parquet数据源
      */
     val parquetDF = spark.read.parquet("hdfs://localhost:9000/huser/resources/users.parquet")
-    parquetDF.select($"name".contains("Al"),$"favorite_numbers".as[Array[Int]].isNotNull)
+    //不使用读取 API 将文件加载到 DataFrame 并进行查询
+    val sqlDF = spark.sql("SELECT * FROM parquet.`hdfs://localhost:9000/huser/resources/users.parquet`")
+    parquetDF.select($"name".contains("Al"), $"favorite_numbers".as[Array[Int]].isNotNull)
+
     /*
       result：
       +------------------+------------------------------+
@@ -50,26 +54,57 @@ object sparkSqlApp {
     parquetDF("name")
     parquetDF.select("name").show()
 
+    //选取部分属性保存到指定的文件夹下
+    parquetDF.select("name", "favorite_color")
+      .write.mode("append").format("json").save(fileLoc + "/test/userCopy")
+    //"mode": append overwrite
+    parquetDF.select($"name", $"favorite_numbers")
+      .write.format("json").mode("overwrite").save(fileLoc + "/test/userCopy")
+    //merge data
+    val combineDF = spark.read.option("mergeSchema", true)
+      .json(fileLoc + "/test/userCopy")
+    parquetDF.where('name.contains("a")).show()
+
     /*
     使用反射推倒Dataset
      */
     val textRDD = sc.textFile("hdfs://localhost:9000/huser/resources/people.txt")
-    val textDF = textRDD.map(line=>line.split(","))
-      .map(attributes=>Person(attributes(0),attributes(1).trim.toInt))
+    val textDF = textRDD.map(line => line.split(","))
+      .map(attributes => Person(attributes(0), attributes(1).trim.toInt))
       .toDF()
-    textDF.map(row=>"Name:"+row.getAs("name")).show()
+    textDF.map(row => "Name:" + row.getAs("name")).show()
 
     /*
     以编程的方式置顶Schema
      */
     //创建StructField并将它包裹在StructType中
-    val schema = StructType(Array(StructField("name",StringType,true),
-      StructField("age",IntegerType,true)))
-    val textRDD1 = textRDD.map(line=>line.split(","))
-      .map(attrs=>Row(attrs(0),attrs(1).trim.toInt))
-    val textDF1 = spark.createDataFrame(textRDD1,schema)//参数1:RDD,参数2:StructType
+    val schema = StructType(Array(StructField("name", StringType, true),
+      StructField("age", IntegerType, true)))
+    val textRDD1 = textRDD.map(line => line.split(","))
+      .map(attrs => Row(attrs(0), attrs(1).trim.toInt))
+    val textDF1 = spark.createDataFrame(textRDD1, schema) //参数1:RDD,参数2:StructType
     textDF1.show()
 
+    //mergeschema
+    val squareDF = sc.makeRDD(1 until 7).map(x => (x, x * x)).toDF("value", "spuare")
+    squareDF.write.parquet(fileLoc + "/resources/key/key=1")
+    val cubeDF = sc.makeRDD(1 until 7).map(x => (x, x * x * x)).toDF("value", "cube")
+    cubeDF.write.parquet(fileLoc + "/resources/key/key=2")
+    val mergeDF = spark.read.option("mergeSchema", true).parquet(fileLoc + "/resources/key")
+
+    //连接数据库
+    val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306")
+      .option("dbtable", "clientDB.client")
+      .option("user", "root")
+      .option("password", "MENG940811linxi**")
+      .load()
+    jdbcDF.show()
+
+    val csvDF = spark.read.option("header",true).option("inferSchema",true).csv(fileLoc+"/resources/shampoo-sales.csv")
+
+    val csvDF2 = csvDF.toDF("month","sales") //改变columnsName
   }
 
 }
